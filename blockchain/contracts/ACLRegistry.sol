0dd90183458a465695f00938c7ef100ba226bffc
Matteo Gisler <matteo.gisler05@gmail.com>
2025-05-20 11:51:13 +0200

    Implement smart contract for acl registry

--- blockchain/contracts/ACLRegistry.sol
+++ blockchain/contracts/ACLRegistry.sol
@@ -1,7 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract ACLRegistry {
-    
-}
+    mapping(bytes32 => mapping(address => bool)) public canAccess;
 
+    event AccessGranted(bytes32 indexed fileId, address indexed grantee);
+
+    function grantAccess(bytes32 fileId, address grantee) external {
+        canAccess[fileId][grantee] = true;
+        emit AccessGranted(fileId, grantee);
+    }
+
+    function checkAccess(bytes32 fileId, address user) external view returns (bool) {
+        return canAccess[fileId][user];
+    }
+}
