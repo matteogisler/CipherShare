3bd8e5159377ca8d5215681f1cfaa13e2c11a0e9
Matteo Gisler <matteo.gisler05@gmail.com>
2025-05-23 17:02:28 +0200

    Small adjust in code structure

--- backend/src/services/ipfs.ts
+++ backend/src/services/ipfs.ts
@@ -4,6 +4,6 @@ const ipfsUrl = process.env.IPFS_API!;
 const client = create({url: ipfsUrl});
 
 export async function addFile(buffer: Buffer): Promise<string> {
-  const {cid} = await client.add(buffer);
+  const { cid } = await client.add(buffer);
   return cid.toString();
 }
